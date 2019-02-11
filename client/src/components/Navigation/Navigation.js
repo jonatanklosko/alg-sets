ce0a1689dbf7e4a4fc46240ecaf5ad60e3a0e0ad
Jonatan Kłosko <jonatanklosko@gmail.com>
2019-02-11 02:51:04 +0100

    Set up left manu.

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
 import AppBar from '@material-ui/core/AppBar';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import Grid from '@material-ui/core/Grid';
 import Icon from '@material-ui/core/Icon';
 import IconButton from '@material-ui/core/IconButton';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 
@@ -27,21 +34,53 @@ const Navigation = () => (
       if (loading) return <div>Fetching data</div>;
       const { me } = data;
       return (
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton color="inherit">
-              <Icon>menu</Icon>
-            </IconButton>
-            <Typography variant="h6" color="inherit" style={{ flexGrow: 1 }}>
-              News
-            </Typography>
-            {me ? (
-              <Avatar src={me.avatar.thumbUrl} />
-            ) : (
-              <Button href="/oauth/sign-in" color="inherit">Sign in</Button>
-            )}
-          </Toolbar>
-        </AppBar>
+        <Fragment>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton color="inherit">
+                <Icon>menu</Icon>
+              </IconButton>
+              <Typography variant="h6" color="inherit" style={{ flexGrow: 1 }}>
+                Alg sets
+              </Typography>
+              {!me && <Button href="/oauth/sign-in" color="inherit">Sign in</Button>}
+            </Toolbar>
+          </AppBar>
+          {me && (
+            <Drawer
+              variant="persistent"
+              anchor="left"
+              open={true}
+            >
+              <Grid container direction="column" alignItems="center" style={{ padding: 8 }}>
+                <Grid item>
+                  <Avatar src={me.avatar.thumbUrl} />
+                </Grid>
+                <Grid item style={{ marginTop: 8 }}>
+                  <Typography variant="subtitle2">{me.name}</Typography>
+                </Grid>
+              </Grid>
+              <Divider />
+              <List>
+                <ListItem button>
+                  <ListItemIcon><Icon>folder</Icon></ListItemIcon>
+                  <ListItemText primary="My alg sets" />
+                </ListItem>
+                <ListItem button>
+                  <ListItemIcon><Icon>folder_special</Icon></ListItemIcon>
+                  <ListItemText primary="Starred" />
+                </ListItem>
+              </List>
+              <Divider />
+              <List>
+                <ListItem button>
+                  <ListItemIcon><Icon>exit_to_app</Icon></ListItemIcon>
+                  <ListItemText primary="Sign out" />
+                </ListItem>
+              </List>
+            </Drawer>
+          )}
+        </Fragment>
       );
     }}
   </Query>
