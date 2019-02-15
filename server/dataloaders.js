321e615895844e3e3d2ee7105bf6b679dd3d63a5
Jonatan Kłosko <jonatanklosko@gmail.com>
2019-02-15 02:25:18 +0100

    Add another dataloader.

--- server/dataloaders.js
+++ server/dataloaders.js
@@ -10,7 +10,17 @@ const batchAlgSets = async (AlgSets, ids) => {
   return ids.map(id => algSets.find(algSet => algSet._id.equals(id)));
 };
 
+const batchUsersByStarredId = async (Users, starredIds) => {
+  const users = await Users.find({ starredAlgSetIds: { $in: starredIds } }).toArray();
+  return starredIds.map(starredId =>
+    users.filter(user =>
+      user.starredAlgSetIds.some(starredAlgSetId => starredAlgSetId.equals(starredId))
+    )
+  );
+};
+
 module.exports = ({ Users, AlgSets }) => ({
   userLoader: new DataLoader(ids => batchUsers(Users, ids), { cacheKeyFn: key => key.toString() }),
   algSetLoader: new DataLoader(ids => batchAlgSets(AlgSets, ids), { cacheKeyFn: key => key.toString() }),
+  usersByStarredAlgSetIdLoader: new DataLoader(ids => batchUsersByStarredId(Users, ids), { cacheKeyFn: key => key.toString() }),
 });
--- server/resolvers/AlgSet.js
+++ server/resolvers/AlgSet.js
@@ -3,7 +3,7 @@ module.exports = {
   creator: async (parent, args, { dataloaders: { userLoader } }) => {
     return await userLoader.load(parent.creatorId);
   },
-  stargazers: async (parent, args, { mongo: { Users } }) => {
-    return await Users.find({ starredAlgSetIds: parent._id }).toArray();
+  stargazers: async (parent, args, { dataloaders: { usersByStarredAlgSetIdLoader } }) => {
+    return await usersByStarredAlgSetIdLoader.load(parent._id);
   },
 };
