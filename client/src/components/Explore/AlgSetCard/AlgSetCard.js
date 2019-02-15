489c39f478ab29b9de91053e923f813e9ff6ba9d
Jonatan Kłosko <jonatanklosko@gmail.com>
2019-02-15 01:18:52 +0100

    Update apollo cache when an alg set is starred or unstarred.

--- client/src/components/Explore/AlgSetCard/AlgSetCard.js
+++ client/src/components/Explore/AlgSetCard/AlgSetCard.js
@@ -12,10 +12,20 @@ import Grid from '@material-ui/core/Grid';
 import Icon from '@material-ui/core/Icon';
 import IconButton from '@material-ui/core/IconButton';
 
+import { STARRED_ALG_SETS_QUERY } from '../../StarredAlgSetList/StarredAlgSetList';
+
 const STAR_ALG_SET_MUTATION = gql`
   mutation StarAlgSet($id: ID!) {
     starAlgSet(id: $id) {
       id
+      name
+      algs
+      createdBy {
+        name
+        avatar {
+          thumbUrl
+        }
+      }
       stargazers {
         id
       }
@@ -56,6 +66,15 @@ const AlgSetCard = ({ algSet, currentUserId }) => {
         <Mutation
           mutation={starredByCurrentUser ? UNSTAR_ALG_SET_MUTATION : STAR_ALG_SET_MUTATION}
           variables={{ id: algSet.id }}
+          update={(store, { data: { starAlgSet, unstarAlgSet } }) => {
+            try {
+              const queryData = store.readQuery({ query: STARRED_ALG_SETS_QUERY });
+              queryData.me.starredAlgSets = starredByCurrentUser
+                ? queryData.me.starredAlgSets.filter(({ id }) => id !== unstarAlgSet.id)
+                : queryData.me.starredAlgSets.concat(starAlgSet);
+              store.writeQuery({ query: STARRED_ALG_SETS_QUERY, data: queryData })
+            } catch(error) {}
+          }}
         >
           {(toggleStar, { error, loading }) => (
             <IconButton disabled={!currentUserId || loading} onClick={toggleStar}>
