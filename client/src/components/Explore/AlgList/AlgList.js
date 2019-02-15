d2c48b9aa7f7a9b7ffffd9281b5adabbbbe59791
Jonatan Kłosko <jonatanklosko@gmail.com>
2019-02-15 19:33:07 +0100

    Add reload button to the random algs list.

--- client/src/components/Explore/AlgList/AlgList.js
+++ client/src/components/Explore/AlgList/AlgList.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
-import Grid from '@material-ui/core/Grid/Grid';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Icon from '@material-ui/core/Icon';
 import LinearProgress from '@material-ui/core/LinearProgress';
 
 import AlgCard from '../../AlgCard/AlgCard';
@@ -14,19 +16,27 @@ const RANDOM_ALGS_QUERY = gql`
 
 const AlgList = () => (
   <Query query={RANDOM_ALGS_QUERY} fetchPolicy="no-cache">
-    {({ error, loading, data }) => {
+    {({ error, loading, data, refetch }) => {
       if (error) return <div>Error</div>
       if (loading) return <LinearProgress />
       const { randomAlgs: algs } = data;
 
       return (
-        <Grid container spacing={8}>
-          {algs.map((alg, index) => (
-            <Grid item key={index} xs={12} md={6} lg={3}>
-              <AlgCard alg={alg} isOwner={false} />
-            </Grid>
-          ))}
-        </Grid>
+        <Fragment>
+          <Grid container spacing={8}>
+            {algs.map((alg, index) => (
+              <Grid item key={index} xs={12} md={6} lg={3}>
+                <AlgCard alg={alg} isOwner={false} />
+              </Grid>
+            ))}
+          </Grid>
+          <div style={{ marginTop: 8, textAlign: 'right' }}>
+            <Button onClick={() => refetch()} variant="contained">
+              Reload
+              <Icon>refresh</Icon>
+            </Button>
+          </div>
+        </Fragment>
       );
     }}
   </Query>
